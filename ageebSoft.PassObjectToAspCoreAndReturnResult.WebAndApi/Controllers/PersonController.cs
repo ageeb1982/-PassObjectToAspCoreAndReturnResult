8f6eb897ca6c3e2c399a076da45907f4db5a7ee1
Mohamed Khidir Ageeb <ageeb1982@gmail.com>
2020-02-10 12:11:30 +0300

    update2020_02_10_12h_11m

---
+++ ageebSoft.PassObjectToAspCoreAndReturnResult.WebAndApi/Controllers/PersonController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ageebSoft.PassObjectToAspCoreAndReturnResult.WebAndApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ageebSoft.PassObjectToAspCoreAndReturnResult.WebAndApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        // GET: api/Person
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            return People;
+        }
+
+        // GET: api/Person/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return People.FirstOrDefault(x=>x.Id==id).Name;
+        }
+
+        // POST: api/Person
+        [HttpPost]
+        public IEnumerable<Person> Post([FromBody] Person value)
+        {
+            People.Add(value);
+            return People;
+        }
+
+        // PUT: api/Person/5
+        [HttpPut("{id}")]
+        public IEnumerable<Person> Put(int id, [FromBody] string value)
+        {
+            var p=People.FirstOrDefault(x => x.Id == id);
+            p.Name = value;
+            return People;
+        }
+
+
+        private List<Person> People = new List<Person>
+        {
+            new Person{Id=1,Name="Mohamed ageeb",Gender=Gender.Male,Salary=10},
+            new Person{Id=2,Name="AbuBaker Saleh",Gender=Gender.Male,Salary=20},
+            new Person{Id=3,Name="Omer Altyeb",Gender=Gender.Male,Salary=30},
+            new Person{Id=4,Name="Osman omer",Gender=Gender.Male,Salary=40},
+            new Person{Id=5,Name="Ali Jubara",Gender=Gender.Male,Salary=50},
+            new Person{Id=6,Name="Abdullah Rabee",Gender=Gender.Male,Salary=50},
+            new Person{Id=7,Name="AbduRahman Omer",Gender=Gender.Male,Salary=60},
+            new Person{Id=8,Name="AlHarih Amjad",Gender=Gender.Male,Salary=70},
+            new Person{Id=9,Name="Mansor Omer",Gender=Gender.Male,Salary=80},
+            new Person{Id=10,Name="Khaled Zuhir",Gender=Gender.Male,Salary=90},
+
+        };
+
+  
+
+
+        [HttpPost(nameof(NewPersonStr))]
+        public List<Person> NewPersonStr( Person person)
+        {
+            //var QueryString = HttpContext.Request.QueryString;
+            //var body = HttpContext.Request.Body;
+            //var header = HttpContext.Request.Headers;
+            
+
+            try
+            {
+                Person person1 = (Person)Convert.ChangeType(person, typeof(Person));
+
+                People.Add(person1);
+            }
+            catch
+            {
+
+            }
+            return People;
+        }
+
+        [HttpGet(nameof(SrchById))]
+        public Person SrchById(int Id)
+        {
+            var ee = HttpContext.Request.QueryString;
+            return People.FirstOrDefault(x => x.Id == Id);
+        }
+
+        [HttpGet(nameof(SrchByName))]
+        public List<Person> SrchByName(string Name)
+        {
+            
+            var ee = HttpContext.Request.QueryString;
+            return People.Where(x =>  x.Name.Contains(Name, StringComparison.OrdinalIgnoreCase)).ToList();
+
+        }
+
+
+        [HttpGet(nameof(SrchBySalary))]
+
+        public List<Person> SrchBySalary(double Salary)
+        {
+            var ee = HttpContext.Request.QueryString;
+            return People.Where(x => x.Salary == Salary).ToList();
+
+        }
+
+        [HttpGet(nameof(SrchByTwoSalary))]
+        public List<Person> SrchByTwoSalary(double Salary1, double Salary2)
+        {
+            var ee = HttpContext.Request.QueryString;
+            double max = 0, min = 0;
+            if(Salary1>Salary2)
+            {
+                max = Salary1;
+                min = Salary2;
+            }
+            else
+            {
+                max = Salary2;
+                min = Salary1;
+            }
+
+
+
+            return People.Where(x => x.Salary <= max && x.Salary>=min).ToList();
+
+        }
+
+
+    }
+}
--- ageebSoft.PassObjectToAspCoreAndReturnResult.WebAndApi/wwwroot/js/site.js
+++ ageebSoft.PassObjectToAspCoreAndReturnResult.WebAndApi/wwwroot/js/site.js
@@ -1,4 +1,115 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿/// <reference path="../lib/jquery/dist/jquery.min.js" />
 
-// Write your JavaScript code.
+ 
+
+var myResult = "";
+$(function () {
+    
+
+    $('.newStr').click(function () {
+        //جديد نصي
+        var obj = {
+            id: 15,
+            name: "Post By JavaScript Object Test",
+          gender:1,
+            salary: 30
+        };
+        
+         var myJSON = JSON.stringify(obj);
+         
+        var myurl = "/api/Person/NewPersonStr";
+        AjX(myurl, myJSON, 'POST', ".newStrRes");
+         
+    });
+
+    $('.SrchById').click(function () {
+        //تسلسلي
+        var obj = {id:5};
+
+        var myurl = "/api/Person/SrchById";
+        AjX(myurl, obj, "GET", ".SrchByIdRes" );
+
+         
+    });
+
+    $('.SrchByName').click(function () {
+        //بحث بالاسم
+        var obj = { name: "omer" }; 
+
+        var myurl = "/api/Person/SrchByName";
+         AjX(myurl, obj, "GET",".SrchByNameRes");
+         
+    });
+
+    $('.SrchBySal').click(function () {
+        //راتب
+        var obj = { salary:50};
+
+        var myurl = "/api/Person/SrchBySalary";
+         AjX(myurl, obj, "GET",".SrchBySalRes" );
+         
+    });
+
+    $('.SrchBy2Sal').click(function () {
+        //راتبين
+        var sal1 = 10;
+        var sal2 = 50;
+        var obj = { salary1: sal1, salary2: sal2 };
+        var myurl = "/api/Person/SrchByTwoSalary";
+        AjX(myurl, obj, "GET",".SrchBySal2Res" );
+       
+    });
+
+
+
+    function GetList(obj) {
+
+        var myUl = document.createElement("ul");
+        $.each(obj, function (index, v1) {
+            var liX = document.createElement('li');
+            var liText = "";
+            if (!isNaN(index)) {
+                var Str = "";
+                $.each(v1, function (iX, vObj) {
+                    Str = Str + iX + ":" + vObj + " ; ";
+                });
+                Str = "{" + Str + "}";
+                liText = document.createTextNode(Str);
+            }
+            else {
+                liText = document.createTextNode(index + ":" + v1 + " ");
+            }
+            
+            
+            
+            liX.appendChild(liText);
+            myUl.appendChild(liX);
+
+        });
+        return myUl;
+    }
+
+
+    function AjX(myUrl, myObj, myType,myRes) {
+        //var Res =
+            $.ajax({
+            url: myUrl,
+            type: myType, //Get,Post
+            contentType: "application/json; charset=utf-8",
+             data: myObj,
+                success: function (response) {
+                    window.console.log(response);
+                     
+                        var list = GetList(response);
+                        $(myRes).html(list); 
+                     
+
+            },
+                error: function (xhr) {
+                    window.console.error(xhr);
+                    $(myRes).html(xhr);
+            }
+        });
+        //return Res;
+    }
+});
